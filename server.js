2678a42011c41224246bcdcf52040e80bf6dc6f5
sathiyamurthy2199 <sathiyamurthykumar@gmail.com>
2018-08-24 12:12:17 +0530

    commited now

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 // Connecting to the database
-mongoose.connect(dbConfig.url)
+mongoose.connect(dbConfig.url,  {useNewUrlParser: true})
 .then(() => {
     console.log("Successfully connected to the database");    
 }).catch(err => {
