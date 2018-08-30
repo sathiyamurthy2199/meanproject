23f6007555b9586d41e3215c86688a33cf9e8bd2
sathiyamurthy2199 <sathiyamurthykumar@gmail.com>
2018-08-30 11:08:36 +0530

    all deleted...!

---
+++ client/js/services/authservices.js
@@ -0,0 +1,21 @@
+ angular.module('app').factory('LoginService', function($http, $rootScope, $location, $q) {
+    
+    return {
+      login: login
+    };
+    function login(username,password){
+      var deferred = $q.defer();
+      // return username == 'sathiya' && password == '100133' ? true : false;
+      $http({
+                url: 'http://localhost:3000/login',
+                method: "POST",
+                data: {'username': username, 'password': password}
+    })
+      .success(function(response) {
+            deferred.resolve(response);
+    }).error(function(err) {
+        deferred.reject(err); 
+    });
+        return deferred.promise;
+    }
+  });
